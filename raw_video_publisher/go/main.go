f2d440c478cafa5d9371d246824ad09990cf4456
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2019-03-29 00:33:29 +0200

    fixed go part

--- raw_video_publisher/go/main.go
+++ raw_video_publisher/go/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"os"
+
 	"./tcp_server"
 	stan "github.com/nats-io/go-nats-streaming"
 )
 
 func main() {
+	nats_endpoint := os.Getenv("NATS_ENDPOINT")
+	if len(nats_endpoint) == 0 {
+		nats_endpoint = "192.168.0.101:4222"
+	}
+
+	nats_cluster_name := os.Getenv("NATS_CLUSTER_NAME")
+	if len(nats_cluster_name) == 0 {
+		nats_cluster_name = "test-cluster"
+	}
+
+	nats_client_id := os.Getenv("NATS_CLIENT_ID")
+	if len(nats_client_id) == 0 {
+		nats_client_id = "cl-1"
+	}
+
+	url_option := stan.NatsURL(nats_endpoint)
 	server := tcp_server.New("0.0.0.0:8080")
-	sc, _ := stan.Connect("test-cluster", "clientID")
+	sc, _ := stan.Connect(nats_cluster_name, nats_client_id, url_option)
 
 	server.OnNewMessage(func(c *tcp_server.Client, message string) {
 		sc.Publish("foo", []byte(message[:len(message)-1]))
