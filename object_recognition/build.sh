e8c04655328200dca2e4309a81abc86aabcff946
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2019-03-29 23:38:52 +0200

    usa makefile instead of shell script

--- object_recognition/build.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-docker build -t fogoh-obj-recognition .
-TAG=$(docker images | grep -w "^fogoh-obj-recognition" | awk '{print $3}')
-docker tag $TAG adikabintang/fogoh-obj-recognition:latest
-docker push adikabintang/fogoh-obj-recognition:latest
--- raw_video_publisher/go/build.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-docker build -t fogoh-raw-vid-pub-go .
-TAG=$(docker images | grep -w "^fogoh-raw-vid-pub-go" | awk '{print $3}')
-docker tag $TAG adikabintang/fogoh-raw-vid-pub-go:latest
-docker push adikabintang/fogoh-raw-vid-pub-go:latest
-
