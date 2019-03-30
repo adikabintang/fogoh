b035742bb2a0e2de502621747cba6155424be14d
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2019-03-30 12:36:50 +0200

    add nats

---
+++ kubernetes_files/nats/install_nats_operator.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+kubectl apply -f https://github.com/nats-io/nats-operator/releases/download/v0.4.3/00-prereqs.yaml
+kubectl apply -f https://github.com/nats-io/nats-operator/releases/download/v0.4.3/10-deployment.yaml
+kubectl apply -f https://raw.githubusercontent.com/nats-io/nats-streaming-operator/master/deploy/default-rbac.yaml
+kubectl apply -f https://raw.githubusercontent.com/nats-io/nats-streaming-operator/master/deploy/deployment.yaml
