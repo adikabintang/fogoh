c7cdcefab03f360b4c7f12c5b648dd898ebd52bf
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2019-05-14 17:19:59 +0300

    add nats cluster

--- kubernetes_files/nats/install_nats_operator.sh
+++ kubernetes_files/nats/install_nats_operator.sh
@@ -3,3 +3,4 @@ kubectl apply -f https://github.com/nats-io/nats-operator/releases/download/v0.4
 kubectl apply -f ./nats_operator/10-deployment.yaml
 kubectl apply -f https://raw.githubusercontent.com/nats-io/nats-streaming-operator/master/deploy/default-rbac.yaml
 kubectl apply -f ./nats_operator/deployment.yaml
+kubectl apply -f ./nats.yaml
